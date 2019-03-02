44636b6c4e2871622bdda4e15c8a8515dcd86107
agalarcon <agalarcon369@gmail.com>
2019-03-02 16:44:51 -0500

    Updated for Python 3.5

--- Modified_RFD_python_Pi.py
+++ Modified_RFD_python_Pi.py
@@ -85,7 +85,7 @@ checkOK = ''
 # ser = serial.Serial(port = port, baudrate = baud, timeout = timeout)
 pic_interval = 60
 extension = ".jpg"
-folder = "/home/pi/RFD_Pi_Code/%s/" % strftime("%m%d%Y_%H%M%S")
+folder = "/home/osboxes/PAYLOADTESTFOLDER/%s/" % strftime("%m%d%Y_%H%M%S")
 dir = os.path.dirname(folder)
 if not os.path.exists(dir):
     os.mkdir(dir)
@@ -110,64 +110,13 @@ cam_vflip = True                       # global variable for camera vertical fli
 
 # GPIO settings for camera mux
 #GPIO.setmode(GPIO.BOARD)        # use board numbering for GPIO header vs broadcom **** broadcom used in adafruit library dependant stuff ****
-GPIO.setup(selection, GPIO.OUT)         # mux "select"
-GPIO.setup(enable1, GPIO.OUT)           # mux "enable1"
-GPIO.setup(enable2, GPIO.OUT)           # mux "enable2"
+#GPIO.setup(selection, GPIO.OUT)         # mux "select"
+#GPIO.setup(enable1, GPIO.OUT)           # mux "enable1"
+#GPIO.setup(enable2, GPIO.OUT)           # mux "enable2"
 
 
-def enable_camera_A():
-    global cam_hflip
-    global cam_vflip
-    global camera_annotation
-    GPIO.output(selection, False)
-    GPIO.output(enable2, True)          # pin that needs to be high set first to avoid enable 1 and 2 being low at same time
-    GPIO.output(enable1, False)         # if coming from a camera that had enable 2 low then we set enable 1 low on next camera
-    #GPIO.output(enable2, True)         # first, we would have both enables low at the same time
-    cam_hflip = True
-    cam_vflip = True
-    camera_annotation = ''
-    time.sleep(0.5)
-    return
-
-def enable_camera_B():
-    global cam_hflip
-    global cam_vflip
-    global camera_annotation
-    GPIO.output(selection, True)
-    GPIO.output(enable2, True)
-    GPIO.output(enable1, False)
-    #GPIO.output(enable2, True)
-    cam_hflip = False
-    cam_vflip = False
-    camera_annotation = 'Camera B'
-    time.sleep(0.5)                        # ??? are these delays going to mess with timming else where ???
-    return
-
-def enable_camera_C():
-    global cam_hflip
-    global cam_vflip
-    global camera_annotation
-    GPIO.output(selection, False)
-    GPIO.output(enable1, True)           # make sure first enable pin to be changed is going high
-    GPIO.output(enable2, False)
-    cam_hflip = False
-    cam_vflip = False
-    camera_annotation = 'Camera C'
-    time.sleep(0.5)
-    return
-
-def enable_camera_D():
-    global cam_hflip
-    global cam_vflip
-    global camera_annotation
-    GPIO.output(selection, True)
-    GPIO.output(enable1, True)
-    GPIO.output(enable2, False)
-    cam_hflip = False
-    cam_vflip = False
-    camera_annotation = 'Camera D'
-    time.sleep(0.5)
-    return
+##Place Camera Functions here - Andrew
+#enable_camera_A()
 
 
 def initOLED():
@@ -251,7 +200,7 @@ def UpdateDisplay():
                 else:
                     break
         except:
-            print "Error with Display Update"
+            print("Error with Display Update")
         draw.rectangle((0,0,width,height), outline=0, fill=0)
         draw.text((0, 0), FirstLine,  font=font, fill=255)
         draw.text((0,15), FourthLine, font=font, fill=255)
@@ -294,7 +243,7 @@ def smile():
     except:
         return
     
-def reset_cam():
+def reset_cam(): #Change this to apply to Chameleon Camera - Andrew
     global width
     global height
     global sharpness
@@ -351,7 +300,7 @@ def sync():
         synccheck = ser.read()
         if(synctry == 0):
             if (synccheck == ""):
-                print "SyncError"
+                print("SyncError")
                 break
         synctry -= 1
     time.sleep(0.5)
@@ -365,10 +314,10 @@ def send_image(exportpath, wordlength):
     trycnt = 0
     outbound = image_to_b64(exportpath)
     size = len(outbound)
-    print size,": Image Size"
-    print "photo request received"
+    print(size,": Image Size")
+    print("photo request received")
     while(cur < len(outbound)):
-        print "Send Position:", cur," // Remaining:", int((size - cur)/1024), "kB"
+        print("Send Position:", cur," // Remaining:", int((size - cur)/1024), "kB")
         checkours = gen_checksum(outbound,cur)
         ser.write(checkours)
         sendword(outbound,cur)
@@ -381,14 +330,14 @@ def send_image(exportpath, wordlength):
             if(trycnt < 3):
                 sync()
                 trycnt += 1
-                print "try number:", trycnt
-                print "resending last @", cur
-                print "ours:",checkours
+                print("try number:", trycnt)
+                print("resending last @", cur)
+                print("ours:",checkours)
             else:
-                print "error out"
+                print("error out")
                 cur = len(outbound)
-    print "Image Send Complete"
-    print "Send Time =", (time.time() - timecheck)
+    print("Image Send Complete")
+    print("Send Time =", (time.time() - timecheck))
     return
 
 
@@ -419,56 +368,56 @@ imagenumber = 0
 recentimg = ""
 reset_cam()
 starttime = time.time()
-print "Startime @ ",starttime
+print("Startime @ ",starttime)
 checkpoint = time.time()
 
-enable_camera_A()          # initialize the camera to something so mux is not floating
 
 
 while(True):
-    print "RT:",int(time.time() - starttime),"Watching Serial"
+    print("RT:",int(time.time() - starttime),"Watching Serial")
     UpdateDisplay()
-    command = ser.read()
+    # command = ser.read()
+    command = 0
     if (command == '1'):
         ser.write('A')
         try:
-            print "Send Image Command Received"
+            print("Send Image Command Received")
             UpdateDisplay()
             #sync()
-            print "Sending:", recentimg
+            print("Sending:", recentimg)
             ser.write(recentimg)
             send_image(folder+recentimg, wordlength)
         except:
-            print "Send Recent Image Error"
+            print("Send Recent Image Error")
     if (command == '2'):
         ser.write('A')
         try:
-            print "data list request recieved"
+            print("data list request recieved")
             UpdateDisplay()
             #sync()
             file = open(folder+"imagedata.txt","r")
-            print "Sending imagedata.txt"
+            print("Sending imagedata.txt")
             for line in file:
                 ser.write(line)
-                #print line
+                #print(line)
             file.close()
             time.sleep(1)
         except:
-            print "Error with imagedata.txt read or send"
+            print("Error with imagedata.txt read or send")
     if (command == '3'):
         ser.write('A')
         try:
-            print"specific photo request recieved"
+            print("specific photo request recieved")
             UpdateDisplay()
             sync()
             imagetosend = ser.read(15)
             send_image(folder+imagetosend,wordlength)
         except:
-            print "Send Specific Image Error"
+            print("Send Specific Image Error")
     if (command == '4'):
         ser.write('A')
         try:
-            print "Attempting to send camera settings"
+            print("Attempting to send camera settings")
             UpdateDisplay()
             #sync()
             file = open(folder+"camerasettings.txt","r")
@@ -478,14 +427,14 @@ while(True):
                 temp = file.read()
             ser.write("\r")
             file.close()
-            print "Camera Settings Sent"
+            print("Camera Settings Sent")
         except:
-            print "cannot open file/file does not exist"
+            print("cannot open file/file does not exist")
             reset_cam()
     if (command == '5'):
         ser.write('A')
         try:
-            print "Attempting to update camera settings"
+            print("Attempting to update camera settings")
             UpdateDisplay()
             file = open(folder+"camerasettings.txt","w")
             temp = ser.read()
@@ -493,36 +442,36 @@ while(True):
                 file.write(temp)
                 temp = ser.read()
             file.close()
-            print "New Camera Settings Received"
+            print("New Camera Settings Received")
             ser.write('A')
             checkpoint = time.time()
         except:
-            print "Error Retrieving Camera Settings"
+            print("Error Retrieving Camera Settings")
             reset_cam()
     if (command == '6'):
             ser.write('A')
-            print "Ping Request Received"
+            print("Ping Request Received")
             UpdateDisplay()
             try:
                 termtime = time.time() + 10
                 pingread = ser.read()
                 while ((pingread != 'D') & (pingread != "")&(termtime > time.time())):
                     if (pingread == 'P'):
-                        print "Ping Received"
+                        print("Ping Received")
                         ser.flushInput()
                         ser.write('P')
                     else:
-                        print "pingread = ",pingread
+                        print("pingread = ",pingread)
                         ser.flushInput()
                         ser.write('A')
                     pingread = ser.read()
                     sys.stdin.flush()
             except:
-                print "Ping Runtime Error"
+                print("Ping Runtime Error")
     if (command == '7'):
         ser.write('A')
         try:
-            print "Attempting to send piruntimedata"
+            print("Attempting to send piruntimedata")
             UpdateDisplay()
             #sync()
             file = open(folder+"piruntimedata.txt","r")
@@ -532,124 +481,102 @@ while(True):
                 temp = file.readline()
             #ser.write("\r")
             file.close()
-            print "piruntimedata.txt sent"
+            print("piruntimedata.txt sent")
         except:
-            print "error sending piruntimedata.txt"
+            print("error sending piruntimedata.txt")
 
 # ------  camera/mux commands  --------
 
     if (command == '8'):             # enable camera a
         ser.write('A')
         try:
-            print 'command received to enable camera A, attempting to enable camera A'
+            print('command received to enable camera A, attempting to enable camera A')
             enable_camera_A()
             #time.sleep(2)
-            print 'returned from enabling camera A'
+            print('returned from enabling camera A')
 
         except:
-            print 'Not done, need to implement catch condition for enable camera A'
+            print('Not done, need to implement catch condition for enable camera A')
 
-    if (command == '9'):             # enable camera b
-        ser.write('A')
-        try:
-            print 'command received to enable camera B, attempting to enable camera B'
-            enable_camera_B()
-            #time.sleep(2)
-            print 'returned from enabling camera B'
-
-        except:
-            print 'Not done, need to implement catch condition for enable camera B'
-
-    if (command == 'c'):             # enable camera c
-        ser.write('A')
-        try:
-            print 'command received to enable camera C, attempting to enable camera C'
-            enable_camera_C()
-            #time.sleep(2)
-            print 'returned from enabling camera C'
-
-        except:
-            print 'Not done, need to implement catch condition for enable camera C'
-            
     if (command == 'd'):             # enable camera d
         ser.write('A')
         try:
-            print 'command received to enable camera D, attempting to enable camera D'
+            print('command received to enable camera D, attempting to enable camera D')
             enable_camera_D()
             #time.sleep(2)
-            print 'returned from enabling camera D'
+            print('returned from enabling camera D')
 
         except:
-            print 'Not done, need to implement catch condition for enable camera D'
+            print('Not done, need to implement catch condition for enable camera D')
 # -----  end of camera commands  -----------------
 
     if (command == 'T'):
         ser.write('A')
         try:
-            print "Time Sync Request Recieved"
+            print("Time Sync Request Recieved")
             
             timeval=str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"))+"\n"
             for x in timeval:
                 ser.write(x)
         except:
-            print "error with time sync"
+            print("error with time sync")
     
     if (checkpoint < time.time()):
-        UpdateDisplay()
-        camera = picamera.PiCamera()
-        try:
-            file = open(folder+"camerasettings.txt","r")
-            width = int(file.readline())
-            height = int(file.readline())
-            sharpness = int(file.readline())
-            brightness = int(file.readline())
-            contrast = int(file.readline())
-            saturation = int(file.readline())
-            iso = int(file.readline())
-            file.close()
-            print "Camera Settings Read"
-        except:
-            print "cannot open file/file does not exist"
-            reset_cam()
-        camera.sharpness = sharpness
-        camera.brightness = brightness
-        camera.contrast = contrast
-        camera.saturation = saturation
-        camera.iso = iso
-        #camera.annotate_text = "Image:" + str(imagenumber)
-        camera.resolution = (2592,1944)
-        extension = '.png'
-        camera.hflip = cam_hflip
-        camera.vflip = cam_vflip
-        camera.annotate_background = picamera.Color('black')
-        camera.annotate_text = camera_annotation
-        #camera.start_preview()
-        smile()
-        camera.capture(folder+"%s%04d%s" %("image",imagenumber,"_a"+extension))
-        print "( 2592 , 1944 ) photo saved"
-        #UpdateDisplay()
-        fh = open(folder+"imagedata.txt","a")
-        fh.write("%s%04d%s @ time(%s) settings(w=%d,h=%d,sh=%d,b=%d,c=%d,sa=%d,i=%d)\n" % ("image",imagenumber,"_a"+extension,str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")),2592,1944,sharpness,brightness,contrast,saturation,iso))
-        camera.resolution = (width,height)
-        extension = '.jpg'
-        camera.hflip = cam_hflip
-        camera.vflip = cam_vflip
-        camera.annotate_text = camera_annotation
-        #smile()
-        camera.capture(folder+"%s%04d%s" %("image",imagenumber,"_b"+extension))
-        print "(",width,",",height,") photo saved"
-        UpdateDisplay()
-        fh.write("%s%04d%s @ time(%s) settings(w=%d,h=%d,sh=%d,b=%d,c=%d,sa=%d,i=%d)\n" % ("image",imagenumber,"_b"+extension,str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")),width,height,sharpness,brightness,contrast,saturation,iso))
-        print "settings file updated"
-        #camera.stop_preview()
-        camera.close()
-        #print "camera closed"
-        recentimg = "%s%04d%s" %("image",imagenumber,"_b"+extension)
-        #print "resent image variable updated"
-        fh.close()
-        #print "settings file closed"
-        print "Most Recent Image Saved as", recentimg
-        imagenumber += 1
+        # UpdateDisplay()
+        # camera = picamera.PiCamera()
+        # try:
+        #     file = open(folder+"camerasettings.txt","r")
+        #     width = int(file.readline())
+        #     height = int(file.readline())
+        #     sharpness = int(file.readline())
+        #     brightness = int(file.readline())
+        #     contrast = int(file.readline())
+        #     saturation = int(file.readline())
+        #     iso = int(file.readline())
+        #     file.close()
+        #     print("Camera Settings Read")
+        # except:
+        #     print("cannot open file/file does not exist")
+        #     reset_cam()
+        # camera.sharpness = sharpness
+        # camera.brightness = brightness
+        # camera.contrast = contrast
+        # camera.saturation = saturation
+        # camera.iso = iso
+        # #camera.annotate_text = "Image:" + str(imagenumber)
+        # camera.resolution = (2592,1944)
+        # extension = '.png'
+        # camera.hflip = cam_hflip
+        # camera.vflip = cam_vflip
+        # camera.annotate_background = picamera.Color('black')
+        # camera.annotate_text = camera_annotation
+        # #camera.start_preview()
+        # smile()
+        # camera.capture(folder+"%s%04d%s" %("image",imagenumber,"_a"+extension))
+        # print("( 2592 , 1944 ) photo saved")
+        # #UpdateDisplay()
+        # fh = open(folder+"imagedata.txt","a")
+        # fh.write("%s%04d%s @ time(%s) settings(w=%d,h=%d,sh=%d,b=%d,c=%d,sa=%d,i=%d)\n" % ("image",imagenumber,"_a"+extension,str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")),2592,1944,sharpness,brightness,contrast,saturation,iso))
+        # camera.resolution = (width,height)
+        # extension = '.jpg'
+        # camera.hflip = cam_hflip
+        # camera.vflip = cam_vflip
+        # camera.annotate_text = camera_annotation
+        # #smile()
+        # camera.capture(folder+"%s%04d%s" %("image",imagenumber,"_b"+extension))
+        # print("(",width,",",height,") photo saved")
+        # UpdateDisplay()
+        # fh.write("%s%04d%s @ time(%s) settings(w=%d,h=%d,sh=%d,b=%d,c=%d,sa=%d,i=%d)\n" % ("image",imagenumber,"_b"+extension,str(datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")),width,height,sharpness,brightness,contrast,saturation,iso))
+        # print("settings file updated")
+        # #camera.stop_preview()
+        # camera.close()
+        # #print("camera closed"
+        # recentimg = "%s%04d%s" %("image",imagenumber,"_b"+extension)
+        # #print("resent image variable updated"
+        # fh.close()
+        # #print("settings file closed"
+        # print("Most Recent Image Saved as", recentimg)
+        # imagenumber += 1
         checkpoint = time.time() + pic_interval
     ser.flushInput()
     ser.flushOutput()
